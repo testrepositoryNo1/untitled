341197c6db64f4c18cf500b041274215f18c830f
Sergey Masuryan <smasuryan@plgn.ru>
2016-09-07 15:23:57 +0500

    find random string

--- main.cpp
+++ main.cpp
@@ -12,11 +12,14 @@
 #include <functional>
 #include <string>
 #include <utility>
-#include <list>
-#include <set>
+//#include <deque>
+//#include <list>
+//#include <set>
+#include <stack>
+#include <queue>
 
 #include <memory>
-
+#include <cassert>
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -26,24 +29,40 @@
 using namespace std;
 
 template <class T>
-void gen(T &v)
+void gen(T &v, size_t size, unsigned int modulo)
 {
-    int r = 0;
-    size_t size = 10;
+  if (modulo > 1) {
+  int r = 0;
     while (size) {
-        r = rand()%1000;
+        r = rand() % modulo;
        v.push_back(r);
        --size;
+    }
+    }
+  else {
+      cout << "modulo must be more or equal 2" << endl;
+      exit(0);
     }
 }
 
 template <class T>
+void gen_(T &v, size_t size, int from)
+{
+    while (size) {
+        v.push_back(from);
+        ++from;
+        --size;
+      }
+}
+
+
+
+template <class T>
 void print(const T &v)
 {
-    auto it = v.begin();
-
-    for (; it != v.end(); ++it)
-        cout << *it << " - " << &(*it) << endl;
+    for (auto a: v)
+      cout << a << " ";
+    cout << endl;
 }
 
 int main ()
@@ -51,30 +70,41 @@ int main ()
     setlocale(0, "");
     srand(time(0));
     srand(rand());
-    ostream_iterator<int> out(cout, " ");
+    ostream_iterator<int> out(cout, "\n");
     clock_t start = clock();
 //----------------------------------------------------------------
 
-    vector<int> vec;
-    gen(vec);
+    size_t count = 0;
+    string str, rand_word = "test" ;
+    size_t x = 0;
+    int r = 0;
 
-    auto x = max_element(vec.begin(), vec.end());
-    print(vec);
-    cout << "max: " << *x << " - " << &(*x) <<endl;
 
-    sort(vec.begin(), vec.end()); /* порядок элементов меняется в ходе сортировки но адреса нет,
-                                    и адрес который получила переменная x указывает на другой элемент (со списком такого не возникает)*/
+    while (str != rand_word) {
+            str = "";
+            x = rand()%10;
+            if (x == rand_word.size()) {
+            while (x){
+                r  = rand() %255;
+                if ( (r >= 65 && r <= 90) || (r >= 97 && r <= 122)  || r == 32 ){
+                    str.push_back(char(r));
+                    --x;
+                  }
+              }
+          }
+            ++count;
+      }
 
-    x = max_element(vec.begin(), vec.end()); /* для коректного отображения максимального элемента */
-    print(vec);
-    cout << "max: " << *x << " - " << &(*x) << endl;
+    cout << str << " - " << count << endl;
 
-//----------------------------------------------------------------
+
+//---------------------------------------------------------------
     clock_t finish = clock();
     double result = (finish - start) * 1.0 / CLOCKS_PER_SEC;
     cout << fixed << result << endl;
     return 0;
 }
+
 
 
 
