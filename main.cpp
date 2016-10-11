07324e92cee963b1a9688dad8b35a28474629210
Sergey Masuryan <smasuryan@plgn.ru>
2016-10-11 14:19:30 +0500

    fixed bug arround milliseconds and microseconds

--- main.cpp
+++ main.cpp
@@ -17,44 +17,21 @@ int main ()
     srand(time(0));
     srand(rand());
     ostream_iterator<int> out(cout, "\n");
-    boost::chrono::microseconds start(clock());
+    boost::chrono::milliseconds start(clock());
 //----------------------------------------------------------------
 
-/*
+
     vector<int> vec;
-    for (int i = 0; i < 1000; ++i )
+    for (int i = 0; i < 10000; ++i )
         vec.push_back(i);
 
-    my_boost_int_Rnd rnd;
-*/
-    shared_ptr<string> s1Ptr(new string ("test"));
-    shared_ptr<string> s2Ptr(new string("this"));
 
-    (*s1Ptr)[0] = 'T';
-    s2Ptr->replace(0, 1, "T");
-
-    vector<shared_ptr<string>> spvec;
-
-    spvec.push_back(s1Ptr);
-    spvec.push_back(s2Ptr);
-    spvec.push_back(s1Ptr);
-    spvec.push_back(s2Ptr);
-    spvec.push_back(s1Ptr);
-    spvec.push_back(s2Ptr);
-
-    for (auto ptr: spvec)
-        cout << *ptr << endl;
-
-    *s1Ptr = "understand";
-
-    for (auto ptr: spvec)
-        cout << *ptr << endl;
 
 
 
 //---------------------------------------------------------------
-    boost::chrono::microseconds end(clock());
-    using ms = boost::chrono::microseconds;
+    boost::chrono::milliseconds end(clock());
+    using ms = boost::chrono::milliseconds;
     ms d = boost::chrono::duration_cast<ms>(end - start);
     double dd = d.count();
     cout << dd / 1000.0 << " sec. = " << end - start << endl;
