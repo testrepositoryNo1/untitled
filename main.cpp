0e096ed5a7be24a7a8aa327b6cd20bbe43f291c9
Sergey Masuryan <smasuryan@plgn.ru>
2017-03-22 14:40:23 +0500

    try to insert all af this to class

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include <net/if.h>
 #include <netinet/if_ether.h>
 #include <bitset>
+#include <cstdlib>
 #include "stdfx.h"
 
 
@@ -29,7 +30,7 @@ string int_to_byte_string(int i)
 int byte_string_to_int(const string& byte_port)
 {
       int int_port = 0;
-      for (int i = 0, j = byte_port.size(); i < byte_port.size(); ++i, --j) {
+      for (size_t i = 0, j = byte_port.size(); i < byte_port.size(); ++i, --j) {
               if (byte_port.at(i) == '1'){
                       int_port += pow(2, j - 1);
                   }
@@ -37,10 +38,43 @@ int byte_string_to_int(const string& byte_port)
       return int_port;
 }
 
+//<Таймстемп захвата пакета> <Адрес назначения> <Порт назначения> <размер полезных данных в UDP пакете>
 
-
-int main()
+class Pcap_dump
 {
+    pair<long, long> timestamp;
+    string DestAddr;
+    uint DestPort;
+    uint usefulDataSize;
+public:
+    Pcap_dump() : timestamp{make_pair(0,0)}, DestAddr{"0.0.0.0"},
+                  DestPort{0}, usefulDataSize{0} {}
+    Pcap_dump(pair<long, long> tmstp, string DestA, uint DestP, uint uDataSz);
+    void show()
+    {
+         DestAddr.pop_back();
+         cout << "TimeStamp: " << timestamp.first << "." << timestamp.second << ", "
+              << "Destination: " << DestAddr << ", "
+              << ", Port: " << DestPort << ", "
+              << "Useful data size: " << usefulDataSize << " bytes" << endl;
+     }
+};
+
+Pcap_dump::Pcap_dump(pair<long, long> tmstp, string DestA, uint DestP, uint uDataSz)
+{
+    timestamp = tmstp;
+    DestAddr = DestA;
+    DestPort = DestP;
+    usefulDataSize = uDataSz;
+}
+
+
+
+
+int main(int argc, char* argv[])
+{
+
+
       string file = "data2.pcap";
       char errbuff[PCAP_ERRBUF_SIZE]; // Create an char array to hold the error.
       pcap_t * pcap = pcap_open_offline(file.c_str(), errbuff); //Open the file and store result in pointer to pcap_t
@@ -48,34 +82,42 @@ int main()
       const u_char *data;
       size_t packetCount = 0;
       string byte_port; /* binary performance */
+      string DestAddress;
+      uint DestPort = 0;
       vector<int> vec;
+      //Pcap_dump object;
+
 
       //Loop through packets and print them to screen
       while (int returnValue = pcap_next_ex(pcap, &header, &data) >= 0) {
-              for (u_int i=0; (i < header->caplen ) ; i++) {
+              for (u_int i=0; i < header->caplen; ++i) {
                       int dt = data[i];
                       vec.push_back(dt);
                   }
-              int Protocol = vec.at(23); // this is thr protocol byte
-              // Protocol = 17 for UDP
+              int Protocol = vec.at(23); // this is the protocol byte
+              // for UDP Protocol must be 17
               if (Protocol == 17) {
-                      //<Таймстемп захвата пакета> <Адрес назначения> <Порт назначения> <размер полезных данных в UDP пакете>
-
-                      cout << dec << "No." << ++packetCount << ", ";
-                      cout << "TimeStamp: "
-                           << header->ts.tv_sec << "."  /* Seconds.  */
-                           << header->ts.tv_usec  << " seconds, "; /* Microseconds.  */
-                      cout << "Destination: ";
+                     /* cout << dec << "No." << ++packetCount << ", ";
+                      /*cout << "TimeStamp: "
+                           << header->ts.tv_sec << "."
+                           << header->ts.tv_usec  << " seconds, "; */
+                     /* cout << "Destination: ";*/
                       for(size_t i = 30; i <= 33; ++i) {
-                              cout << dec << vec.at(i) << ".";
+                              //cout << dec << vec.at(i) << ".";
+                              DestAddress += to_string(vec.at(i)) + ".";
                           }
-
                       byte_port =  int_to_byte_string(vec.at(36));
                       byte_port += int_to_byte_string(vec.at(37));
-                      int port = byte_string_to_int(byte_port);
-                      cout << "\b, Port: " << port << ", ";
+                      DestPort = byte_string_to_int(byte_port);
+                      /*cout << "\b, Port: " << DestPort << ", ";
                       cout << "Useful data size: "
-                           << header->len - 42 << " bytes" << endl;
+                           << header->len - 42 << " bytes" << endl;*/
+
+                      Pcap_dump object(make_pair(header->ts.tv_sec, header->ts.tv_usec),
+                                          DestAddress,
+                                          DestPort,
+                                          (header->len - 42));
+                      object.show();
                     // Show a warning if the length captured is different
                       if (header->len != header->caplen) {
                               cout << "Warning! Capture size different than packet size: "
@@ -96,6 +138,7 @@ int main()
                             //cout.unsetf (ios::hex);
                         }
                       cout << "\n\n" << flush;*/
+
                       vec.clear();
                       vec.shrink_to_fit();
                   }
@@ -104,5 +147,9 @@ int main()
                       vec.clear();
                       vec.shrink_to_fit();
                   }
+              DestAddress = "";
           }
+
+
+      return 0;
 }
